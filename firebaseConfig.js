4f9cb82b31e868b3e10a1a14eee4741e3035f5fa
Patrick Ferguson <pfergie2016@gmail.com>
2024-07-24 23:56:20 -0500

    a

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {}
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDB7zjsR_qbHE_ITsFsRHcFyXARVvLN7bs",
+  authDomain: "mapapp-e985b.firebaseapp.com",
+  projectId: "mapapp-e985b",
+  storageBucket: "mapapp-e985b.appspot.com",
+  messagingSenderId: "94896775000",
+  appId: "1:94896775000:web:e9784090eff2a9e1c04f2d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
